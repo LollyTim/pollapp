c0d5c2c87fdef673c715e8d4defe7631978a0ec1
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-05-11 00:50:14 +0100

    supabase

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,30 @@
 import { Link, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { Alert, FlatList, StyleSheet, Text, View } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import { useEffect, useState } from 'react';
+import { supabase } from '../lib/supabase';
 
 
-const polls = [{ id: 1 }, { id: 2 }, { id: 3 }]
+// const polls = [{ id: 1 }, { id: 2 }, { id: 3 }]
 export default function HomeScreen() {
+    const [polls, setPolls] = useState([])
+
+    useEffect(() => {
+        const fetchPolls = async () => {
+            // console.warn("Fetching....")
+
+
+            let { data, error } = await supabase
+                .from('polls')
+                .select('*')
+            if (error) {
+                Alert.alert("Error Fetching Data")
+            }
+            setPolls(data)
+        }
+        fetchPolls()
+    }, [])
     return (
         <>
             <Stack.Screen options={{
