22a265368bbdd8329c80085142608c5c918fa3a6
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-05-12 01:27:26 +0100

    create poll

--- src/app/polls/new.tsx
+++ src/app/polls/new.tsx
@@ -1,16 +1,45 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
-import { Redirect, Stack } from 'expo-router'
+import { Redirect, Stack, router } from 'expo-router'
 import { Feather } from '@expo/vector-icons';
 import { useAuth } from '../../providers/AuthProvider';
+import { supabase } from '../../lib/supabase';
 
 const CreatePoll = () => {
 
-    const [question, setQuestion] = useState('')
-    const [options, setOptions] = useState(["", ""])
+    const [question, setQuestion] = useState('');
+    const [options, setOptions] = useState(["", ""]);
+    const [error, setError] = useState("")
     const { session, user } = useAuth();
 
-    const createpoll = () => { console.warn("Create") }
+    const createpoll = async () => {
+        setError("")
+        if (!question) {
+            setError("Please enter a question")
+            return;
+        }
+        const validOptions = options.filter(o => !!o);
+        if (validOptions.length < 2) {
+            setError("Please provide at least 2 valid options")
+            return;
+        }
+
+
+        const { data, error } = await supabase
+            .from('polls')
+            .insert([
+                { question, options },
+            ])
+            .select()
+        if (error) {
+            Alert.alert("Failed to create the poll")
+            console.log(error)
+        }
+        router.back();
+
+        console.warn("Create");
+
+    }
     if (!user) {
         return <Redirect href={"/login"} />
     }
@@ -40,6 +69,7 @@ const CreatePoll = () => {
             ))}
             <Button title='Add options' onPress={() => setOptions([...options, ""])} />
             <Button onPress={createpoll} title='Create Poll' />
+            <Text style={{ color: "red", fontSize: 18 }}>{error}</Text>
         </View>
     )
 }
